d8f5243d8c7b3925d27af46559dcfd1e4555223e
Adam Hörnemalm <ahornemalm@clearstreet.io>
2024-12-01 21:50:41 +0100

    Day 1

--- src/main/java/aoc/day01/Day01.java
+++ src/main/java/aoc/day01/Day01.java
@@ -3,19 +3,61 @@ package aoc.day01;
 import aoc.Day;
 import aoc.Utils;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Scanner;
+import java.util.concurrent.atomic.AtomicReference;
+import java.util.stream.Collectors;
 
 public class Day01 implements Day {
 
     @Override
     public String part1(String input) {
-        List<String> lines = Utils.splitLines(input);
-        return lines.isEmpty() ? "" : lines.get(0);
+      List<String> lines = Utils.splitLines(input);
+
+      ArrayList<Integer> leftSide = new ArrayList<>(), rightSide = new ArrayList<>();
+      for (String loc : lines) {
+        var x = loc.split(" {3}");
+        leftSide.add(Integer.valueOf(x[0]));
+        rightSide.add(Integer.valueOf(x[1]));
+      }
+
+      leftSide.sort(Integer::compareTo);
+      rightSide.sort(Integer::compareTo);
+
+      int totalDistance = 0;
+      for (int i = 0; i < leftSide.size(); i++) {
+        totalDistance += Math.abs(leftSide.get(i) - rightSide.get(i));
+      }
+
+      return String.valueOf(totalDistance);
     }
 
     @Override
     public String part2(String input) {
-        return input;
+      List<String> lines = Utils.splitLines(input);
+
+      ArrayList<Integer> leftSide = new ArrayList<>(), rightSide = new ArrayList<>();
+      for (String loc : lines) {
+        var x = loc.split(" {3}");
+        leftSide.add(Integer.valueOf(x[0]));
+        rightSide.add(Integer.valueOf(x[1]));
+      }
+
+      int similarityScore = 0;
+      for (Integer value : leftSide) {
+        int occurence = 0;
+        for (Integer integer : rightSide) {
+          if (value.equals(integer)) {
+            occurence++;
+          }
+        }
+        similarityScore += value * occurence;
+      }
+
+    return String.valueOf(similarityScore);
     }
 
 }
--- src/test/java/aoc/day01/Day01Test.java
+++ src/test/java/aoc/day01/Day01Test.java
@@ -3,6 +3,9 @@ package aoc.day01;
 
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
 import java.util.Collections;
 import java.util.List;
 
@@ -11,26 +14,26 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 public class Day01Test {
 
     @Test
-    public void testPart1(){
+    public void testPart1() throws IOException {
         // Given
-        String input = "test";
+        String input = Files.readString(Path.of("src/main/resources/testDay01.txt"));
 
         // When
         String result = new Day01().part1(input);
 
         // Then
-        assertEquals(input, result);
+        assertEquals(11, Integer.valueOf(result));
     }
 
     @Test
-    public void testPart2(){
+    public void testPart2() throws IOException {
         // Given
-        String input = "test";
+        String input = Files.readString(Path.of("src/main/resources/testDay01.txt"));
 
         // When
         String result = new Day01().part2(input);
 
         // Then
-        assertEquals(input, result);
+        assertEquals(31, Integer.valueOf(result));
     }
 }
